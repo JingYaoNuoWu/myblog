feacf33e1520e82ef498e36d53b37ddc904b536a
Raindayly <fppuz123456@163.com>
2022-10-02 23:46:29 +0800

    scroll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,54 @@
 <template>
   <div class="common-layout">
+      <el-header>Header</el-header>
+
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside id="side-bar" width="200px">
+        <div id="aside-wapper">
+          <div style="background-color: #000000ff;height: 100vh;"></div>
+        </div>
+      </el-aside>
+
       <el-container>
         <el-main>
-
-            <div class="main-wapper">
-        <el-header>Header</el-header>
-
-            </div></el-main>
+          <div class="main-wapper">
+          </div>
+        </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from "@vue/reactivity";
+import { ref } from "@vue/reactivity";
 
-
-  let a  = ref(1);
-
-  
-
-  
+let a = ref(1);
 </script>
 
 <style>
-.main-wapper{
+.main-wapper {
   min-height: 100vh;
+}
+.el-container .el-aside {
+  width: 260px;
+}
+.el-container .el-main {
+  padding: 0;
+  padding-left: calc((100% - 1100px)/2);
+}
+.el-header {
+  position: fixed;
+  top:0;
+  right: 0;
+  left: 260px;
+  min-width: 100vh;
+}
+#side-bar{
+  max-height: 100vh;
+}
+#aside-wapper{
+  width: 100%;
+  overflow-y: auto;
+
 }
 </style>
