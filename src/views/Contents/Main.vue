0368f01755a4d6c74ecab67f9bf88b8a2a8b10a0
Raindayly <fppuz123456@163.com>
2022-10-08 21:00:06 +0800

    makedown yuque --failure

--- src/views/Contents/Main.vue
+++ src/views/Contents/Main.vue
@@ -1,10 +1,7 @@
 <!-- 根据动态路由匹配参数后，生成对应的html填进template中 -->
 <template>
-    <button @click="b">123213</button>
-    <div contenteditable="true" style="outline:none">
-        <span>123</span>
-        <span>123</span>
-        <span>123</span>
+    <div ref="editableContent" contenteditable="true" style="outline:none">
+
         <div>12312
 
         </div>
@@ -12,33 +9,76 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from 'vue';
+import { logicalExpression } from '@babel/types';
+import { onMounted, Ref, ref } from 'vue';
 // import {markdown} from 'markdown'
-    const a = ref("")
-    let nowElement : HTMLElement
-    function b (){
-    // let out = markdown.toHTML(a.value)
-    let preModifyText:string
-    let ModifyText:string
-    
-        
-    }
-    window.addEventListener("DOMCharacterDataModified",(e)=>{
-        if(e instanceof MutationEvent)
-        let a:HTMLElement = e.target as HTMLElement
-        nowElement = a.parentElement as HTMLElement
-        preModifyText = e. as string
-    })
+const a = ref("")
+let nowElement: MutationObserver
+const editableContent: Ref = ref(null);
+let isSpace = false;
+let oldValue: any;
+let newValue: any;
+function modifyFn(a: any, b: any) {
 
-    window.addEventListener("keypress",(e)=>{
-        
-        if(e.key == " "){
-            
-            console.log();
-            
+    if (a instanceof Array && a.length == 1) {
+        oldValue = a[0].oldValue
+        newValue = a[0].target.nodeValue
+        console.log(panduan(oldValue, newValue, a[0].target));
+
+    }
+
+    // for(let item of a){
+
+    // }
+
+}
+function panduan(oldValue: string, newValue: string, ele: HTMLElement) {
+    let oldValueFirstWord = oldValue.substring(0, 1)
+    let oldValueSecondWord = oldValue.substring(1, 2)
+    let newValueFirstWord = newValue.substring(0, 1)
+    let newValueSecondWord = newValue.substring(1, 2)
+
+
+    if (oldValueFirstWord != "#" || newValueFirstWord != "#") return false;
+
+    // if(oldValueSecondWord == " ") return false;
+    if (newValueSecondWord == " " && oldValueSecondWord != " " && isSpace) {
+
+        let parentele = ele.parentElement
+        if (parentele != null) {
+            let text = parentele.innerText
+            let dom = document.createElement("h1")
+            dom.innerText = text
+
+            dom.appendChild(document.createElement("br"))
+            parentele.replaceWith(dom)
+            // dom.setSelectionRange
         }
-    })
-    
+        return true;
+    }
+    return false;
+
+}
+onMounted(() => {
+    const config = { characterData: true, characterDataOldValue: true, subtree: true };
+    const observer = new MutationObserver(modifyFn)
+    observer.observe(editableContent.value, config)
+
+
+
+})
+
+window.addEventListener("keypress", (e) => {
+    if (e.key == " ") {
+        isSpace = true
+    }
+
+
+})
+window.addEventListener("keyup", (e) => {
+    isSpace = false
+})
+
     // console.log( markdown.toHTML( "Hello" ) );
 </script>
 
